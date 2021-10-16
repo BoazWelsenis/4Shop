7830c2b7f1cf2ab735dc6317d1d92e7f6c3d8308
BoazWelsenis <D281029@edu.rocwb.nl>
2021-10-16 11:41:18 +0200

    Bugfix aanpassen van Categorie
    Normaal gezien als je iets Edit wordt alleen een naam aangepast. Nu werd er eerst een Object gemaakt, waardoor als je op opslaan klikt, je een nieuw item aangemaakt wordt wanner je op opslaan klikt. Dit was niet de bedoeling, dus is dit aangepast.

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -74,7 +74,7 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        $category = new Category();
+        // $category = new Category();
         $category->name = $request->name;
         $category->save();
 
